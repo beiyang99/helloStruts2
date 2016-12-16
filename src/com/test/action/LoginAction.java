9d09404b341b7ddcc84a8980fe0131e96e397cad
beiyang99wq <beiyang99wq@163.com>
2016-12-16 16:57:30 +0800

    initial setup

---
+++ src/com/test/action/LoginAction.java
@@ -0,0 +1,27 @@
+package com.test.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport{
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+   public String execute() throws Exception{
+	   return "success";
+   }
+	
+	//对应表单上的
+	private String username;
+	private String password;
+}
+
